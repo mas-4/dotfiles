85fd9963b4b9013776aa8733158dd1f9331ea0eb
malan88 <malan88@standingwater.io>
2020-06-14 10:29:56 -0400

    Add alias, function for vimwiki (see body)
    Initially wanted simple way to open vimwiki direct from command line and
    added `vimwiki` alias. Then thought it would be useful to open my tasks
    file from the command line and added that as an alias `vimtask`. Then
    though I'd also like to interact with the git repo I keep in there from
    anywhere without going into the vimwiki directory so looked into how to
    interact with a git repo from outside the repo, found -C, and then
    decided to write a function. And there we have it folks.
    
    Added:
    - `vimwiki` function
    - `vimtask` alias

---
+++ .zsh.d/vimwiki.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This function allows me to go straight into vimwiki index or interact with the
+# git database without changing directory.
+vimwiki () {
+    if [[ $# == 0 ]]
+    then
+        nvim +'VimwikiIndex'
+    elif [[ $1 == 'git' ]]
+    then
+        git -C ~/vimwiki/ ${@:2}
+    else
+        echo 'Usage: vimwiki [git] [args ...]'
+    fi
+}
