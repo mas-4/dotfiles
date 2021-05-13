ebbede20c1846428dc65982770372d2992c7bc31
malan88 <m@stdwtr.io>
2021-05-12 22:43:11 -0400

    Add ~/bin to path

--- .zshrc.d/environment_variables.sh
+++ .zshrc.d/environment_variables.sh
@@ -4,3 +4,4 @@ export HISTORY_IGNORE='(bg|fg|c|ls|pwd|history|exit|make*|* --help)'
 export PASSWORD_STORE_ENABLE_EXTENSIONS=true
 export GPG_TTY=$(tty)
 export ZSH=$HOME/.oh-my-zsh
+export PATH=$HOME/bin:$PATH
